71474e606b2643dc1f33295042a2f4f73d21a2ef
Nidhi <nidhimehra850@gmail.com>
2024-01-18 12:26:32 +0530

    github data aded

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const express = require('express')
 const app = express()
 const port = 4000
+  employee= {  
+      "name":       "sonoo",   
+      "salary":      56000,   
+      "married":    true  
+  }  
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +15,9 @@ app.get('/', (req, res) => {
 app.get('/login',(req,res)=>{
 res.send("login")
 })
+app.get('/employee',(req,res)=>{
+  res.json(employee)
+  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
